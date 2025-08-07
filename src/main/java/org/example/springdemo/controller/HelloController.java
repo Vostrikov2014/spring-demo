e4c24875c274590addae0b1fc17cb898f5d18456
Vostrikov <vostrikovDA@utgard.cbr.ru>
2025-08-07 18:19:37 +0300

    test continue

--- src/main/java/org/example/springdemo/controller/HelloController.java
+++ src/main/java/org/example/springdemo/controller/HelloController.java
@@ -1,12 +1,34 @@
 package org.example.springdemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for handling requests related to greeting.
+ */
 @RestController
 public class HelloController {
+
+    /**
+     * Returns a greeting message.
+     *
+     * @return A string containing the greeting message "Hello World".
+     */
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
-}
+
+    /**
+     * Returns the length of the provided string.
+     *
+     * @param text The string whose length is to be calculated.
+     * @return The length of the string.
+     */
+    @GetMapping("/length")
+    public int getLength(@RequestParam String text) {
+        return text.length();
+    }
+    }
+
